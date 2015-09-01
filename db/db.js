b6badb276e43c3c2fd8b5f47fd5f3fef6e85b9b5
Anoop Kulkarni <anoop.kulkarni@gmail.com>
2015-09-01 12:04:45 -0700

    mongodb conn

---
+++ db/db.js
@@ -0,0 +1,24 @@
+var MongoClient = require('mongodb').MongoClient;
+var util = require('util');
+var url = 'mongodb://localhost:27017/yamf';
+var db;
+
+MongoClient.connect(url, function(err, mydb) {
+  if(err) throw err;
+  util.log("Connected correctly to server");
+  db = mydb;
+});
+
+module.exports = {
+ 
+ getLogs:function(log, callback){
+
+   var logCollection = db.collection('logs');
+
+   logCollection.findOne(function(err,result){
+     if(err) return callback(err,null);
+     callback(null,result);
+   });
+ }
+
+}
